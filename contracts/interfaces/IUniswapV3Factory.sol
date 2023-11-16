e64c9e21b630db65ed5d91339891c4e315c76d1b
Wsscrypto <Mudoor.eth@proton.me>
2023-11-16 20:38:21 +0800

    翻译Factory接口合约

--- contracts/interfaces/IUniswapV3Factory.sol
+++ contracts/interfaces/IUniswapV3Factory.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
-/// @title The interface for the Uniswap V3 Factory
-/// @notice The Uniswap V3 Factory facilitates creation of Uniswap V3 pools and control over the protocol fees
+/// @title Uniswap V3 Factory 合约接口文件
+/// @notice 用于创建pool，并设置费用
 interface IUniswapV3Factory {
-    /// @notice Emitted when the owner of the factory is changed
-    /// @param oldOwner The owner before the owner was changed
-    /// @param newOwner The owner after the owner was changed
+    /// @notice 当Factory的所有者变更时，发出此事件提示
+    /// @param oldOwner 变更前地址
+    /// @param newOwner 变更后地址
     event OwnerChanged(address indexed oldOwner, address indexed newOwner);
 
-    /// @notice Emitted when a pool is created
-    /// @param token0 The first token of the pool by address sort order
-    /// @param token1 The second token of the pool by address sort order
-    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
-    /// @param tickSpacing The minimum number of ticks between initialized ticks
-    /// @param pool The address of the created pool
+    /// @notice Factory创建Pool的时候，发出此事件提示
+    /// @param token0 经过排序后的 token0
+    /// @param token1 经过排序后的 token1
+    /// @param fee 每次swap都要收的费用，基数是 0.0001 即万分之一，若收0.3%费用，则这里为3000
+    /// @param tickSpacing 两个tick之间的最小间隔
+    /// @param pool pool部署地址
     event PoolCreated(
         address indexed token0,
         address indexed token1,
@@ -23,56 +23,54 @@ interface IUniswapV3Factory {
         address pool
     );
 
-    /// @notice Emitted when a new fee amount is enabled for pool creation via the factory
-    /// @param fee The enabled fee, denominated in hundredths of a bip
-    /// @param tickSpacing The minimum number of ticks between initialized ticks for pools created with the given fee
+    /// @notice 当允许设置新的费用类型时，发出此事件提示
+    /// @param fee 费用，基数是 0.0001
+    /// @param tickSpacing 两个tick之间的最小间隔
     event FeeAmountEnabled(uint24 indexed fee, int24 indexed tickSpacing);
 
-    /// @notice Returns the current owner of the factory
-    /// @dev Can be changed by the current owner via setOwner
-    /// @return The address of the factory owner
+    /// @notice 返回当前Factory的所有者
+    /// @dev 更换Factory所有者
+    /// @return 所有者地址
     function owner() external view returns (address);
 
-    /// @notice Returns the tick spacing for a given fee amount, if enabled, or 0 if not enabled
-    /// @dev A fee amount can never be removed, so this value should be hard coded or cached in the calling context
-    /// @param fee The enabled fee, denominated in hundredths of a bip. Returns 0 in case of unenabled fee
-    /// @return The tick spacing
+    /// @notice 返回某个费用下，tickspacing大小，如果为0，说明给的fee参数是不合法的
+    /// @dev 费用和tickspacing值是不能改变的
+    /// @param 费用
+    /// @return tick spacing
     function feeAmountTickSpacing(uint24 fee) external view returns (int24);
 
-    /// @notice Returns the pool address for a given pair of tokens and a fee, or address 0 if it does not exist
-    /// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
-    /// @param tokenA The contract address of either token0 or token1
-    /// @param tokenB The contract address of the other token
-    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
-    /// @return pool The pool address
+    /// @notice 返回 两个token在某个费率下的 pool 地址
+    /// @dev tokenA 和 B 可能是未经排序的，排序后一般标记为 token0 和 token1
+    /// @param tokenA 
+    /// @param tokenB
+    /// @param 费用
+    /// @return pool 地址
     function getPool(
         address tokenA,
         address tokenB,
         uint24 fee
     ) external view returns (address pool);
 
-    /// @notice Creates a pool for the given two tokens and fee
-    /// @param tokenA One of the two tokens in the desired pool
-    /// @param tokenB The other of the two tokens in the desired pool
-    /// @param fee The desired fee for the pool
-    /// @dev tokenA and tokenB may be passed in either order: token0/token1 or token1/token0. tickSpacing is retrieved
-    /// from the fee. The call will revert if the pool already exists, the fee is invalid, or the token arguments
-    /// are invalid.
-    /// @return pool The address of the newly created pool
+    /// @notice 为 tokenA tokenB 在 fee 费率下创建一个 pool
+    /// @param tokenA
+    /// @param tokenB
+    /// @param fee
+    /// @dev tokenA 和 tokenB 未经排序
+    /// @return 新创建的pool地址
     function createPool(
         address tokenA,
         address tokenB,
         uint24 fee
     ) external returns (address pool);
 
-    /// @notice Updates the owner of the factory
-    /// @dev Must be called by the current owner
-    /// @param _owner The new owner of the factory
+    /// @notice 更新Factory的所有者
+    /// @dev 必须由当前所有者调用
+    /// @param _owner 新的所有者地址
     function setOwner(address _owner) external;
 
-    /// @notice Enables a fee amount with the given tickSpacing
-    /// @dev Fee amounts may never be removed once enabled
-    /// @param fee The fee amount to enable, denominated in hundredths of a bip (i.e. 1e-6)
-    /// @param tickSpacing The spacing between ticks to be enforced for all pools created with the given fee amount
+    /// @notice 设置新的 fee 和 tickspacing 对
+    /// @dev 一旦设置新的费率，则无法删除
+    /// @param fee
+    /// @param tickSpacing 
     function enableFeeAmount(uint24 fee, int24 tickSpacing) external;
 }
