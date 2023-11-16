6d13a0715df4e324afb10f89496f9fbf07905a49
Wsscrypto <Mudoor.eth@proton.me>
2023-11-16 19:27:13 +0800

    翻译禁止委托调用文件

--- contracts/NoDelegateCall.sol
+++ contracts/NoDelegateCall.sol
@@ -1,25 +1,22 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.7.6;
 
-/// @title Prevents delegatecall to a contract
-/// @notice Base contract that provides a modifier for preventing delegatecall to methods in a child contract
+/// @title 用于禁止对某合约功能进行委托调用
+/// @notice 用于禁止对某合约功能进行委托调用
 abstract contract NoDelegateCall {
-    /// @dev The original address of this contract
+    /// @dev 合约的原始部署地址，赋值后不可更改
     address private immutable original;
 
     constructor() {
-        // Immutables are computed in the init code of the contract, and then inlined into the deployed bytecode.
-        // In other words, this variable won't change when it's checked at runtime.
         original = address(this);
     }
 
-    /// @dev Private method is used instead of inlining into modifier because modifiers are copied into each method,
-    ///     and the use of immutable means the address bytes are copied in every place the modifier is used.
+    /// @dev 要求调用函数的主体必须是合约自己
     function checkNotDelegateCall() private view {
         require(address(this) == original);
     }
 
-    /// @notice Prevents delegatecall into the modified method
+    /// @notice 禁止委托调用的修饰器
     modifier noDelegateCall() {
         checkNotDelegateCall();
         _;
